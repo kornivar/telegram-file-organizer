684cdc9f0afdf7af1533899661672bd2bc627fb9
artembobrikov433-rgb <artembobrikov433@gmail.com>
2025-11-06 12:55:57 +0200

    added main function

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ import sys
 from controller import FileSorterController
 from view import CLIView, GUIView
 
+def main():
+    mode = "gui" if len(sys.argv) < 2 else sys.argv[1]
+
+    if mode == "cli":
+        view = CLIView()
+        controller = FileSorterController(view)
+        path = view.get_directory()
+        controller.sort_files(path)
+    else:
+        controller = FileSorterController(None)
+        view = GUIView(controller)
+        controller.view = view
+        view.run()
+
+if __name__ == "__main__":
+    main()
