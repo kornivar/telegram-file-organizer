541793243a7e6a4a1045d6d20d28b49f88d4cd34
Alex-SHv <116091695+Alex-SHv@users.noreply.github.com>
2025-11-06 13:35:38 +0200

    commit_model

--- model.py
+++ model.py
@@ -1,1 +1,16 @@
 print("Hello Model") 
+print("Hello Model") 
+def get_files(self):
+    files = []
+
+    if self.source.exists() and self.source.is_dir():
+        for item in self.source.iterdir():
+            if item.is_file():
+                files.append(item)
+
+        if files:
+            return files
+        else:
+            return f"In the folder '{self.source}' No files."
+    else:
+        return f"Folder '{self.source}' Not found or is not a directory."
